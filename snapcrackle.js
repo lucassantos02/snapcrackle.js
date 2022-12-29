8aff2f33bfb6f5bbed21beaa6a13a1be2db7f7e1
Lucassantos <lucasrodriguessantos200802@gmail.com>
2022-12-28 22:33:27 -0300

    test

---
+++ snapcrackle.js
@@ -0,0 +1,22 @@
+function snapCrackle(maxValue) {
+    let text = "";
+    for (let numb = 1; numb <= maxValue; numb++) {
+        if (numb % 2 !== 0) {
+            text += "Snap, ";
+            continue;
+        }
+        if (numb % 5 === 0) {
+        text += "Crackle, ";
+        continue;
+        }
+        if (numb % 2 !== 0 && numb % 5 === 0) {
+            text += "SnapCrackle, ";
+            continue;
+        }
+        
+        text += numb + ", ";
+    }
+    return text;
+}
+
+snapCrackle();
