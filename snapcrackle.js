dbc82ac9bf3e78124f47e79e776671980049cef8
Lucassantos <lucasrodriguessantos200802@gmail.com>
2022-12-28 22:34:52 -0300

    firt commit

--- snapcrackle.js
+++ snapcrackle.js
@@ -2,15 +2,15 @@ function snapCrackle(maxValue) {
     let text = "";
     for (let numb = 1; numb <= maxValue; numb++) {
         if (numb % 2 !== 0) {
-            text += "Snap, ";
+            text += "Snap , ";
             continue;
         }
         if (numb % 5 === 0) {
-        text += "Crackle, ";
+        text += "Crackle , ";
         continue;
         }
         if (numb % 2 !== 0 && numb % 5 === 0) {
-            text += "SnapCrackle, ";
+            text += "SnapCrackle , ";
             continue;
         }
         
